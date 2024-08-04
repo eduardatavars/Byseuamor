3d7bf58ae473ee626ce92509358c5402a4c8b14f
eduardatavars <mariaeduardtavares@gmail.com>
2024-08-04 10:00:00 -0300

    Sua mensagem de commit

---
+++ js/app.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+    catalogo.eventos.init();
+})
+
+var catalogo = {};
+
+catalogo.eventos = {
+
+    init: () => {
+       catalogo.metodos.obterItensCatalogo();
+    }
+}
+
+catalogo.metodos = {
+
+    // obtem a lista de itens do cardápio
+    obterItensCatalogo: (categoria ='pinturas') => {
+        
+        var filtro = MENU [categoria];
+        console.log(filtro);
+
+        
+
+        $.each(filtro, (i, e) => {
+
+            let temp = catalogo.templates.item.replace(/\${img}/g, e.img)
+            .replace(/\${name}/g, e.name)
+            .replace(/\${price}/g, e.price.toFixed(2).replace('.', ','));
+            $("#itemsCatalogo").append(temp) //adicionando o template
+
+        })
+    },
+}
+
+catalogo.templates = {
+
+    item: `
+        <div class="col-3 mb-5">
+            <div class="card card-item">
+                <div class="img-produto">
+                    <img src="\${img}" width="50rem" height="220rem" alt="Placa de madeira pintada">
+                </div>
+                <p class="title-produto text-center mt-4">
+                    <b>\${name}</b>
+                </p>
+                <p class="price-produto text-center">
+                    <b>R$ \${price}</b>
+                </p>
+                <div class="add-carrinho">
+                    <span class="btn-menos"><i class="fas fa-minus"></i></span>
+                    <span class="add-numero-itens">0</span>
+                    <span class="btn-mais"><i class="fas fa-plus"></i></span>
+                    <span class="btn btn-add"><i class="fas fa-shopping-basket"></i></span>
+                </div>
+            </div>
+        </div>
+    `
+}
---
+++ js/dados.js
@@ -0,0 +1,187 @@
+var MENU = {
+    "pinturas": [
+        {
+            "id": "pinturas01",
+            "img": "./img/Catalogo_Bea/Pinturas/img1.jpeg",
+            "name": "pinturas01",
+            "dsc": "...",
+            "price": 70.00
+        },
+        {
+            "id": "pinturas02",
+            "img": "./img/Catalogo_Bea/Pinturas/img2.jpg",
+            "name": "pinturas02",
+            "dsc": "...",
+            "price": 70.00
+        },
+        {
+            "id": "pinturas03",
+            "img": "./img/Catalogo_Bea/Pinturas/img6.jpg",
+            "name": "pinturas03",
+            "dsc": "...",
+            "price": 70.00
+        },
+        {
+            "id": "pinturas04",
+            "img": "./img/Catalogo_Bea/Pinturas/img4.jpeg",
+            "name": "pinturas04",
+            "dsc": "...",
+            "price": 70.00
+        },
+        {
+            "id": "pinturas05",
+            "img": "./img/Catalogo_Bea/Pinturas/img5.jpg",
+            "name": "pinturas05",
+            "dsc": "...",
+            "price": 70.00
+        },
+    ],
+    "cestas": [
+        {
+            "id": "cestas01",
+            "img": "./img/Catalogo_Bea/Caixas/img2.jpg",
+            "name": "cestas01",
+            "dsc": "...",
+            "price": 70.00
+        },
+        {
+            "id": "cestas02",
+            "img": "./img/Catalogo_Bea/Caixas/img5.jpg",
+            "name": "cestas02",
+            "dsc": "...",
+            "price": 70.00
+        },
+        {
+            "id": "cestas03",
+            "img": "./img/Catalogo_Bea/Caixas/img8.jpg",
+            "name": "cestas03",
+            "dsc": "...",
+            "price": 70.00
+        },
+        {
+            "id": "cestas04",
+            "img": "./img/Catalogo_Bea/Caixas/img3.jpg",
+            "name": "cestas04",
+            "dsc": "...",
+            "price": 70.00
+        },
+        {
+            "id": "cestas05",
+            "img": "./img/Catalogo_Bea/Caixas/img6.jpg",
+            "name": "cestas05",
+            "dsc": "...",
+            "price": 70.00
+        },
+    ],
+    "personalizados": [
+        {
+            "id": "personalizados01",
+            "img": "./img/Catalogo_Bea/Personalizados/img1.jpeg",
+            "name": "personalizados01",
+            "dsc": "...",
+            "price": 70.00
+        },
+        {
+            "id": "personalizados02",
+            "img": "./img/Catalogo_Bea/Personalizados/img7.jpeg",
+            "name": "personalizados02",
+            "dsc": "...",
+            "price": 70.00
+        },
+        {
+            "id": "personalizados03",
+            "img": "./img/Catalogo_Bea/Personalizados/img4.jpeg",
+            "name": "personalizados03",
+            "dsc": "...",
+            "price": 70.00
+        },
+        {
+            "id": "personalizados04",
+            "img": "./img/Catalogo_Bea/Personalizados/img6.jpg",
+            "name": "personalizados04",
+            "dsc": "...",
+            "price": 70.00
+        },
+        {
+            "id": "personalizados05",
+            "img": "./img/Catalogo_Bea/Personalizados/img3.jpeg",
+            "name": "personalizados05",
+            "dsc": "...",
+            "price": 70.00
+        },
+    ],
+    "quadros": [
+        {
+            "id": "quadros01",
+            "img": "./img/Catalogo_Bea/Quadros/img2.jpeg",
+            "name": "quadros01",
+            "dsc": "...",
+            "price": 70.00
+        },
+        {
+            "id": "quadros02",
+            "img": "./img/Catalogo_Bea/Quadros/img6.jpg",
+            "name": "quadros02",
+            "dsc": "...",
+            "price": 70.00
+        },
+        {
+            "id": "quadros03",
+            "img": "./img/Catalogo_Bea/Quadros/img4.jpg",
+            "name": "quadros03",
+            "dsc": "...",
+            "price": 70.00
+        },
+        {
+            "id": "quadros04",
+            "img": "./img/Catalogo_Bea/Quadros/img8.jpg",
+            "name": "quadros04",
+            "dsc": "...",
+            "price": 70.00
+        },
+        {
+            "id": "quadros05",
+            "img": "./img/Catalogo_Bea/Quadros/img1.jpg",
+            "name": "quadros05",
+            "dsc": "...",
+            "price": 70.00
+        },
+    ],
+    "buques": [
+        {
+            "id": "buques01",
+            "img": "./img/Catalogo_Bea/Buques/img1.jpg",
+            "name": "buques01",
+            "dsc": "...",
+            "price": 70.00
+        },
+        {
+            "id": "buques02",
+            "img": "./img/Catalogo_Bea/Buques/img2.jpg",
+            "name": "buques02",
+            "dsc": "...",
+            "price": 70.00
+        },
+        {
+            "id": "buques03",
+            "img": "./img/Catalogo_Bea/Buques/img5.jpg",
+            "name": "buques03",
+            "dsc": "...",
+            "price": 70.00
+        },
+        {
+            "id": "buques04",
+            "img": "./img/Catalogo_Bea/Buques/img6.jpg",
+            "name": "buques04",
+            "dsc": "...",
+            "price": 70.00
+        },
+        {
+            "id": "buques05",
+            "img": "./img/Catalogo_Bea/Buques/img7.jpg",
+            "name": "buques05",
+            "dsc": "...",
+            "price": 70.00
+        },
+    ]
+}
